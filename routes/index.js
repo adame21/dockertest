4f2c266327209034b8d56b014e603cdbcd64ae02
adam <adam210792@gmail.com>
2021-02-08 15:54:09 +0200

    added endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/hey/:word', (req, res) => {
+
+  console.log(`Word entered: ${req.params.word}`);
+
+  res.status(200).send("entered: " + req.params.word)
+
+})
 
 module.exports = router;
